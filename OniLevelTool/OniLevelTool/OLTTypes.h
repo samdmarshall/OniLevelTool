a1c73a801a3d6248ad88ff8e43e8827c0798f629
Sam Marshall <me@samdmarshall.com>
2013-10-17 12:59:46 -0400

    adding missing windows types

--- OniLevelTool/OniLevelTool/OLTTypes.h
+++ OniLevelTool/OniLevelTool/OLTTypes.h
@@ -31,6 +31,10 @@
 #pragma mark Standard Types
 
 #ifdef _WIN32
+	typedef unsigned __int8 uint8_t;
+	typedef signed __int8 int8_t;
+	typedef unsigned __int16 uint16_t;
+	typedef signed __int16 int16_t;
 	typedef unsigned __int32 uint32_t;
 	typedef signed __int32 int32_t;
 	typedef unsigned __int64 uint64_t;
